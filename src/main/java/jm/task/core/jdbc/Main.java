3bf6ff95718f9950e22020bbf5e351aeb0aea204
Egor Trifonov <trifonov-egor@mail.ru>
2021-04-05 14:16:28 +0300

    Remove files

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -6,6 +6,7 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 public class Main {
     public static void main(String[] args) {
         UserServiceImpl userService = new UserServiceImpl();
+
         userService.createUsersTable();
         userService.saveUser("Дима", "Иванов", (byte) 21);
         userService.saveUser("Лёша", "Семёнов", (byte) 19);
